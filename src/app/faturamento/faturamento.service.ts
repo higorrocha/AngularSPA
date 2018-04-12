05b0b4c0f4cbc22d760328df4b828668ff08bde3
higorrocha <higorrocha.14@gmail.com>
2018-04-12 16:02:57 -0300

    Adicionando códigos ao componente faturamento

--- src/app/faturamento/faturamento.service.ts
+++ src/app/faturamento/faturamento.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class FaturamentoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
+  getFaturamento(){
+    return this.http.get(environment.apiUrl + '/faturamento');
+  }
 }
