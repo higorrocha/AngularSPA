6c5dc39bc1f8fe6bd0ae4082f83ddd4d73c0d4c3
higorrocha <higorrocha.14@gmail.com>
2018-04-12 15:43:52 -0300

    Adicionando códigos ao resumo para absorção de dados da API

--- src/app/resumo/resumo.service.ts
+++ src/app/resumo/resumo.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ResumoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getResumo(){
+    return this.http.get
+    (environment.apiUrl + "/resumo");
+  }
 
 }
