53147bb3ba902f7726e91fa1b0d8a6b7e0d439e2
higorrocha <higorrocha.14@gmail.com>
2018-04-12 15:11:08 -0300

    Adicionando código ao componente resumo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
-import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
 import { ResumoComponent } from './resumo/resumo.component';
 
 @NgModule({
