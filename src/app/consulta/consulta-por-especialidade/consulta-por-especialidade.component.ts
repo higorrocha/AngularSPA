4294c14281709806b9717c6e429511bf0dbcdc71
higorrocha <higorrocha.14@gmail.com>
2018-04-12 15:21:07 -0300

    Gerando componente consulta por especialidade

---
+++ src/app/consulta/consulta-por-especialidade/consulta-por-especialidade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-por-especialidade',
+  templateUrl: './consulta-por-especialidade.component.html',
+  styleUrls: ['./consulta-por-especialidade.component.css']
+})
+export class ConsultaPorEspecialidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
