3134d43e6ed18a8d597940d586f89ff18f5f127e
higorrocha <higorrocha.14@gmail.com>
2018-04-12 14:58:33 -0300

    Gerando componente resumo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { ResumoComponent } from './resumo/resumo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelSimplesComponent,
     BarraNavegacaoComponent,
-    MenuLateralComponent
+    MenuLateralComponent,
+    ResumoComponent
   ],
   imports: [
     BrowserModule,
